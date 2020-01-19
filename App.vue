4046f428f67583decaac1cd532197746a326031a
JB <jb.tellez@gmail.com>
2020-01-18 21:09:17 -0800

    adds vue client for blog api

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <h1>Blog Posts</h1>
+        <ul>
+            <li v-for="post in posts" v-bind:key="post.title">
+                <h2>{{ post.title }}</h2>
+                <p>{{ post.author }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+
+    data () {
+        return {
+            posts: []
+        }
+    },
+
+    async mounted () {
+
+        const url = 'http://167.172.117.35:8000/api/v1/'
+
+        const response = await axios.get(url)
+
+        this.posts = response.data
+    }
+}
+</script>
+<style>
+    .container {
+        background-color: aliceblue;
+    }
+    h1 {
+        color: firebrick;
+    }
+</style>
